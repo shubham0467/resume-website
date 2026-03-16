eec139fd3ffa6cf861dd067591759e956d4c1784
Shubham Pal <palshubham25@gmail.com>
2026-03-16 14:59:35 +0530

    update-2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts'; // Import these
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +11,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
-    provideCharts(withDefaultRegisterables())
   ]
 };
